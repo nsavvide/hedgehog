498ab8824adbf4498a1e346c65f36461117484d9
Niels Savvides <niels.i.savvides@gmail.com>
2026-04-17 11:11:23 +0200

    chore: setup ffi

---
+++ core_parser/src/lib.rs
@@ -0,0 +1,33 @@
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+#[repr(C)]
+pub struct ParsedUrl {
+    pub host: *mut c_char,
+    pub path: *mut c_char,
+}
+
+/// no_mangle allows c to find function by name
+// TODO: update url parameter
+#[unsafe(no_mangle)]
+pub extern "C" fn parse_url(_url: *const c_char) -> ParsedUrl {
+    let host = "example.com";
+    let path = "/index.html";
+
+    ParsedUrl {
+        host: CString::new(host).unwrap().into_raw(),
+        path: CString::new(path).unwrap().into_raw(),
+    }
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn free_parsed_url(parsed_url: ParsedUrl) {
+    unsafe {
+        if !parsed_url.host.is_null() {
+            let _ = CString::from_raw(parsed_url.host); // Reclaim ownership to free memory
+        }
+        if !parsed_url.path.is_null() {
+            let _ = CString::from_raw(parsed_url.path); // Reclaim ownership to free memory
+        }
+    }
+}
---
+++ src/main.c
@@ -0,0 +1,20 @@
+#include "rust_bridge.h"
+#include <stdio.h>
+
+int main() {
+  const char *target_url = "http://cracked-dev.com/engine.html";
+
+  printf("C HOST: Sending URL to Rust worker...\n");
+
+  ParsedUrl url = parse_url(target_url);
+
+  printf("C HOST: Received parsed data!\n");
+  printf("  -> Host: %s\n", url.host);
+  printf("  -> Path: %s\n", url.path);
+
+  printf("C HOST: Returning memory to Rust for cleanup...\n");
+  free_parsed_url(url);
+
+  printf("C HOST: Success. Exiting.\n");
+  return 0;
+}
